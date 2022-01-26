a969aa519066228f9eb6e9c1b40226c4d274fc35
Reece Humphreys <reecewh@icloud.com>
2022-01-25 23:29:55 -0500

    Adding structure for FragmentationEvent

---
+++ odap/FragmentationEvent.py
@@ -0,0 +1,19 @@
+from .Satellite import Satellite
+
+class FragmentationEvent():
+
+    _inputMass = 0
+    _outputMass = 0
+
+    def run_simulation():
+        compute_fragment_count()
+
+    def compute_fragment_count():
+        pass
+        
+
+    def n_fragments_dist(self, L_c, S = 1):
+        return 6 * S * (L_c)**(-1.6)
+
+    def __init__(self) -> None:
+        pass
