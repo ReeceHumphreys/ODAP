7144c8230ea7f2317e72646e08ce921c897e4189
Reece Humphreys <reecewh@icloud.com>
2022-02-17 16:12:54 -0500

    Fixed Satellite

--- odap/satellite.py
+++ odap/satellite.py
@@ -1,38 +1,83 @@
-from .Parsing.TLEParser import TLEParser
+import numpy as np
+from .CoordTransforms import coe2rv
+from .utils.utils import circle_area
+
+# kg
+DEFAULT_MASS = 1000.0
+
+# "m3 / (s2)"
+mu_Earth = 3.986004418e14
 
 
 class Satellite:
 
-    def __init__(self, tle):
+    # All satellites should have orbital elements.
+    # Then the carteisan state is constructed if needed
+
+    def __init__(self, tle, mass=DEFAULT_MASS):
         """
-        Constructs all the necessary attributes for the satellite object.
+        Constructs all the necessary attributes for the satellite object from a TLE.
 
         Parameters
         ----------
-            tles : array
-                an (3, ) NumPy array where each element corresponds to the line of a two-line element
+            tles : TLE
+            mass : Mass of the Satellite in kg
 
         """
+        self.tle = tle
+        self.mass = mass
 
-        line1_data, line2_data, line3_data = TLEParser().parse(tle)
+        self.ecc = tle.ecc
+        self.a = tle.a
+        self.inc = tle.inc
+        self.raan = tle.raan
+        self.argp = tle.argp
+        self.n = tle.n
+        self.nu = tle.nu
+        self.M = tle.M
 
-        # Extracting data from the first line of the tle
-        self.name = line1_data
+        # Setting computed properties to None
+        self.r = np.array([None, None, None])
+        self.v = np.array([None, None, None])
 
-        # Extracting data from the second line of the tle
-        (self.num,
-         self.classification,
-         self.year,
-         self.launch,
-         self.piece,
-         self.epoch_year,
-         self.epoch_day,
-         self.ballistic_coefficient,
-         self.mean_motion_dotdot,
-         self.bstar,
-         self.ephemeris_type,
-         self.element_number) = line2_data
+    @property
+    def cartesian_state(self):
+        if self.r.any() is None or self.v.any() is None:
+            p = self.a * (1 - self.ecc ** 2)
+            r, v = coe2rv(
+                mu_Earth,
+                p,
+                self.ecc,
+                np.deg2rad(self.inc),
+                np.deg2rad(self.raan),
+                np.deg2rad(self.argp),
+                np.deg2rad(self.nu),
+            )
+            self.r = r
+            self.v = v
+        return self.r, self.v
 
-        # Extracting data from the third line of the tle
-        (self.inclination, self.raan, self.eccentricity, self.aop,
-         self.mean_anomaly, self.mean_motion, self.epoch_rev) = line3_data
+    # Calculates the Characteristic Length assuming the mass is formed like a
+    # sphere.
+
+    @property
+    def position(self):
+        r, v = self.cartesian_state
+        return r
+
+    @property
+    def velocity(self):
+        r, v = self.cartesian_state
+        return v
+
+    @property
+    def characteristic_length(self):
+        return ((6.0 * self.mass) / (92.937 * np.pi)) ** (1.0 / 2.26)
+
+    @property
+    def area(self):
+        return circle_area(self.characteristic_length)
+
+    @property
+    def area_to_mass_ratio(self):
+        return self.area / self.mass
