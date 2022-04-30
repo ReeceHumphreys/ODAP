1cdab338b299cef76109941f58d6ac9383ea2f57
Reece Humphreys <reecewh@icloud.com>
2022-04-30 13:58:49 -0400

    Formatting notebooks with black

--- notebooks/data/planetary_data.py
+++ notebooks/data/planetary_data.py
@@ -1,10 +1,10 @@
-G = 6.6743015e-11        # [m3⋅kg−1⋅s−2]
+G = 6.6743015e-11  # [m3⋅kg−1⋅s−2]
 
 earth = {
-    'name': 'Earth',
-    'mass': 5.9722e24,        # [kg]
+    "name": "Earth",
+    "mass": 5.9722e24,  # [kg]
     # [m3⋅s−2] (Using more precise value from wikipedia rather than computing)
-    'mu': 3.9860044188e14,
-    'radius': 6.3781e6,  # [m]
-    'J2': 0.0010826267  # []
+    "mu": 3.9860044188e14,
+    "radius": 6.3781e6,  # [m]
+    "J2": 0.0010826267,  # []
 }
